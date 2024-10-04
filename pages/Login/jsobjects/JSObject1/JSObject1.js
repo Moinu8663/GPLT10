dbda099aa668df2786ef77c0878f318e5cd3aa1c
mdshaikh241516 <mdshaikh241516@gmail.com>
2024-10-04 19:37:14 +0000

    updated

--- pages/Login/jsobjects/JSObject1/JSObject1.js
+++ pages/Login/jsobjects/JSObject1/JSObject1.js
@@ -3,16 +3,17 @@ export default {
 		const response = await Query1.run();
 		if(response.length > 0) {
 			const user = response[0]; // Generate a token with expiration (simplified example)
-			const expiresAt = new Date().getTime() + 50 * 60 * 1000; // 50 minutes from now
+			const expiresAt = new Date().getTime() + 360 * 60 * 1000; // 50 minutes from now
 			const tokenPayload = {
 				mobileno: user.mobile_no,
 				password: user.user_password,
-				expiresAt: expiresAt };
+				expiresAt: expiresAt 
+			};
 			const token = btoa(JSON.stringify(tokenPayload));
 			// Store session and token
 			storeValue('user_session', user);
 			storeValue('auth_token', token);       
-			console.log(token);
+			//console.log(token);
 			// Show success message
 			showAlert('Login Successful!', 'success');
 			navigateTo('AuctionDashBoard');
